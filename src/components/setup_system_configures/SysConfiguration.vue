dc48a0f1df12e1fb036f4a6fa19131ffbff5d480
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2020-09-29 17:37:57 +0600

    system configuration
    not complete

---
+++ src/components/setup_system_configures/SysConfiguration.vue
@@ -0,0 +1,283 @@
+<template>
+  <div>
+     <div v-if="$system_variables.status_task_loaded==1">
+    <List v-show="method=='list'"/>    
+    <AddEdit v-show="method=='add' || method=='edit'"/> 
+    </div>
+  </div>
+  
+  
+</template>
+
+<script>
+import List from './List.vue'
+import AddEdit from './AddEdit.vue'
+export default {
+    name: 'SystemConfiguration',
+    components: {
+        List,AddEdit   
+    },
+    mounted:function()
+    {
+       this.$system_variables.labels_task=this.$system_functions.load_languages([
+        {language:this.$system_variables.language,file:'components/setup_system_configures/language.js'}
+        
+      ]); 
+      //Object.assign(this.item, this.default_item); 
+      this.init();        
+    },
+    data() {
+      return {
+        method:'list',        
+        permissions:{},
+        columns:{hidden_columns:[],control_columns:[],filter_columns:{},display_columns:[]},
+        items:[],
+        types:[],
+        item:{},        
+        default_item:{id:0,name:"",ordering:99,status:'Active',action_0:0,action_1:0,action_2:0,action_3:0,action_4:0,action_5:0,action_6:0,action_7:0,action_8:0},
+        //pagination:{current_page:1,items_per_page:50,num_itemshowing:0,num_items:10,page_options: [10,20, 50, 100, 500]},
+        pagination:{current_page:1,items_per_page:5,num_item_showing:0,num_items:0},
+        max_modules_tasks_level:1,
+        modules_tasks:[],
+        max_module_task_action:9,
+        reload_items:true,
+        loaded_role:false
+      }
+    },
+    watch: {
+        $route(to, from) {
+        this.routing(to);      
+        }
+    },
+    methods:{
+    routing:function(route)
+    {      
+      if(route.path=='/setup_system_configures')
+      {
+        this.method='list';
+        this.get_items();
+      }
+      else if(route.path=="/setup_system_configures/add")
+      {
+        this.method='add';
+        this.add_edit(0);
+      }
+      else if(route.path.indexOf("/setup_system_configures/edit/")!=-1)
+      {
+        this.method='edit';        
+        this.add_edit(route.params['item_id']);        
+      }
+    },
+    init:function()
+    { 
+        this.$system_variables.status_task_loaded=0;  
+        this.$system_variables.status_data_loaded=1;  
+        this.reload_items=true;
+        var form_data=new FormData();
+        form_data.append ('token_auth', this.$system_variables.user.token_auth);      
+        this.$axios.post('/setup_system_configures/initialize',form_data)
+        .then(response=>{
+            if(response.data.error_type)        
+            {            
+                this.$system_variables.status_task_loaded=0;
+                this.$bvToast.toast(this.$system_variables.get_label(response.data.error_type), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+            }
+            else
+            {
+                this.permissions=response.data.permissions;                 
+                Object.assign(this.default_item, response.data.default_item);                 
+                Object.assign(this.item, this.default_item); 
+                if(response.data.hidden_columns)  
+                {
+                  this.columns.hidden_columns=response.data.hidden_columns;
+                }               
+                if(response.data.max_module_task_action)  
+                {
+                  this.max_module_task_action=response.data.max_module_task_action;
+                }               
+                this.max_modules_tasks_level=response.data.max_modules_tasks_level;
+                this.modules_tasks=response.data.modules_tasks;  
+                this.$system_variables.status_task_loaded=1;
+
+                this.set_control_columns();
+                this.set_filter_columns();
+                this.set_display_columns();  
+                this.routing(this.$route);
+            }        
+        })
+        .catch(error => {     
+            console.log(error);
+            this.$system_variables.status_task_loaded=-1;
+        });
+    },
+    set_control_columns:function()
+    {
+      //value field mandatory for checked value
+      var control_columns=[]
+      control_columns.push({ label:this.$system_variables.get_label('label_id'), value: 'id',selected: this.columns.hidden_columns.indexOf('id')>=0?'id':''});
+      control_columns.push({ label:this.$system_variables.get_label_task('label_purpose'), value: 'purpose',selected: this.columns.hidden_columns.indexOf('purpose')>=0?'purpose':''});
+      control_columns.push({ label:this.$system_variables.get_label_task('label_description'), value: 'description',selected: this.columns.hidden_columns.indexOf('description')>=0?'description':''});
+      control_columns.push({ label:this.$system_variables.get_label_task('label_config_value'), value: 'config_value',selected: this.columns.hidden_columns.indexOf('config_value')>=0?'config_value':''});
+      control_columns.push({ label:this.$system_variables.get_label('label_status'), value: 'status',selected: this.columns.hidden_columns.indexOf('status')>=0?'status':''});
+
+      this.columns.control_columns=control_columns;      
+    },
+    set_filter_columns:function()
+    {
+      var filter_columns={};
+      filter_columns['id']={filter_type:'number',fitler_from:{value:"", default_value:""},fitler_to:{value:"", default_value:""}};
+      filter_columns['purpose']={value:"", default_value:""};//default value not using               
+      filter_columns['description']={value:"", default_value:""};//default value not using               
+      filter_columns['config_value']={value:"", default_value:""};//default value not using               
+      filter_columns['status']={filter_type:'list', options:['Active','In-Active'], value:"", default_value:""};
+      //filter_columns['date_created']={filter_type:'date',fitler_start:{value:'', default_value:""},fitler_end:{value:'', default_value:""}};
+      this.columns.filter_columns=filter_columns;      
+    },
+    set_display_columns:function()
+    {
+      var columns={};
+      columns['actions']={label:this.$system_variables.get_label('label_action'), hidden:this.columns.false,sticky_column:false};//cannot set it sticky becuase dropdown will goes bellow
+      columns['id']={label:this.$system_variables.get_label('label_id'), hidden:this.columns.hidden_columns.indexOf('id')>=0?true:false,sticky_column:true,sortable:false};
+      columns['purpose']={label:this.$system_variables.get_label_task('label_purpose'), hidden:this.columns.hidden_columns.indexOf('purpose')>=0?true:false,sticky_column:false,sortable:false};
+      columns['description']={label:this.$system_variables.get_label_task('label_description'), hidden:this.columns.hidden_columns.indexOf('description')>=0?true:false,sticky_column:false,sortable:false};
+      columns['config_value']={label:this.$system_variables.get_label_task('label_config_value'), hidden:this.columns.hidden_columns.indexOf('config_value')>=0?true:false,sticky_column:false,sortable:false};
+      columns['status']={label:this.$system_variables.get_label_task('label_status'), hidden:this.columns.hidden_columns.indexOf('status')>=0?true:false,sticky_column:false,sortable:false};
+      this.columns.display_columns=this.$system_functions.get_display_columns(columns); 
+    },
+    get_items:function()
+    {
+        if(this.reload_items)
+        {
+            this.$system_variables.status_data_loaded=0;        
+            var form_data=new FormData();
+            form_data.append ('token_auth', this.$system_variables.user.token_auth);                  
+            this.$axios.post('/setup_system_configures/get_items',form_data)
+            .then(response=>{          
+              this.$system_variables.status_data_loaded=1;
+              if(response.data.error_type)        
+              {   
+                  this.$bvToast.toast(this.$system_variables.get_label(response.data.error_type), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+              }
+              else
+              {   
+                  
+                  this.items=response.data.items;
+                  //this.pagination.num_items=response.data.num_items;//here returning all data                                        
+                  this.pagination.num_items=this.items.length;
+                  this.reload_items=false;
+              }       
+            })
+            .catch(error => {   
+                this.$system_variables.status_data_loaded=1;
+                this.$bvToast.toast(this.$system_variables.get_label("Response Error"), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+            });
+        }
+    },
+    add_edit:function(item_id)
+    {
+      if(item_id>0)
+      {
+        if(!(this.permissions.action_2))
+        { 
+          this.$system_variables.status_task_loaded=-2;
+        }
+        else
+        {
+          this.$system_variables.status_data_loaded=0;        
+          var form_data=new FormData();
+          form_data.append ('token_auth', this.$system_variables.user.token_auth);                  
+          form_data.append ('item_id', item_id);
+          this.$axios.post('/setup_system_configures/get_item',form_data)
+          .then(response=>{          
+            this.$system_variables.status_data_loaded=1;
+            if(response.data.error_type)        
+            {            
+              this.$bvToast.toast(this.$system_variables.get_label(response.data.error_type), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+              this.$router.push("/setup_system_configures");
+            }
+            else
+            {
+              if(response.data.item)  
+              {
+                this.item=response.data.item; 
+              }
+              else
+              {
+                this.$bvToast.toast(this.$system_variables.get_label('Data Not Found'), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+                this.$router.push("/setup_system_configures");
+              }
+            }        
+          })
+          .catch(error => {   
+            this.$system_variables.status_data_loaded=1;
+            this.$bvToast.toast(this.$system_variables.get_label("Response Error"), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});  
+            this.$router.push("/setup_system_configures");            
+          });
+        }
+      }
+      else
+      {
+        if(!(this.permissions.action_1))
+        {
+          this.$system_variables.status_task_loaded=-2;
+        }
+        else
+        { 
+          this.item={};//need to reset       
+          Object.assign(this.item, this.default_item);          
+        }        
+      } 
+       
+    },
+    role:function(item_id)
+    {
+      if(item_id>0)
+      {
+        if(!(this.permissions.action_2))
+        { 
+          this.$system_variables.status_task_loaded=-2;
+        }
+        else
+        {
+          this.loaded_role=false;//for reload role task
+          this.$system_variables.status_data_loaded=0;        
+          var form_data=new FormData();
+          form_data.append ('token_auth', this.$system_variables.user.token_auth);                  
+          form_data.append ('item_id', item_id);
+          this.$axios.post('/setup_system_configures/get_item',form_data)
+          .then(response=>{          
+            this.$system_variables.status_data_loaded=1;
+            if(response.data.error_type)        
+            {            
+              this.$bvToast.toast(this.$system_variables.get_label(response.data.error_type), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+              this.$router.push("/setup_system_configures");
+            }
+            else
+            {
+              if(response.data.item)  
+              {
+                this.item=response.data.item; 
+                this.loaded_role=true;//reseted
+              }
+              else
+              {
+                this.$bvToast.toast(this.$system_variables.get_label('Data Not Found'), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});
+                this.$router.push("/setup_system_configures");
+              }
+            }        
+          })
+          .catch(error => {   
+            this.$system_variables.status_data_loaded=1;
+            this.$bvToast.toast(this.$system_variables.get_label("Response Error"), {title: this.$system_variables.get_label('label_error'),variant:'danger',autoHideDelay: 5000,appendToast: false});  
+            this.$router.push("/setup_system_configures");            
+          });
+        }
+      }
+      else
+      {
+        this.$system_variables.status_task_loaded=-2;        
+      }
+    },    
+  }
+}
+</script>
---
+++ src/components/setup_system_configures/language.js
@@ -0,0 +1,30 @@
+export var labels={
+    label_sys_system_configuration_title:{
+        en:"System Configuration",
+        bn:'কনফিগারেশন '       
+    },
+    label_sys_system_configuration_list:{
+        en:"List",
+        bn:'তালিকা '       
+    },
+    label_sys_system_configuration_action_add:{
+        en:"Create New",
+        bn:'নতুন '       
+    },
+    label_sys_system_configuration_action_edit:{
+        en:"Edit",
+        bn:'সম্পাদন '       
+    },
+    label_purpose:{
+        en:"Purpose",
+        bn:'Purpose'       
+    },    
+    label_description:{
+        en:"Description",
+        bn:'Description'       
+    },  
+    label_config_value:{
+        en:"Value",
+        bn:"Description"
+    }, 
+}
